81b7b763e2ff532fbcad02bdf31b07847f96b67f
Nabil Enayet <enayetn@gmail.com>
2015-05-06 17:47:28 -0400

    Adding pixl files
    Adding relevant pixl files...

---
+++ static/js/lib/pixl.js
@@ -0,0 +1,59 @@
+(function ( $ ) {
+	
+	$.fn.pixl = function (height, width, pixl) {
+
+		this.empty();
+
+		this.css( "height", height );
+		this.css( "width", width );
+		this.css( "text-align", "center" );
+		this.css( "background-color", "grey" );
+		this.css( "line-height", "0" );
+
+		var pixlSize;
+		var pixlMargin;
+		var pixlSizeName = "";
+		
+		switch (pixl) {
+			case 1:
+				pixlSizeClass = "ten"
+			pixlSize = 10;
+			pixlMargin = 2;
+				break;
+			case 2:
+				pixlSizeClass = "twenty"
+			pixlSize = 16;
+			pixlMargin = 4;
+				break;
+			case 3:
+				pixlSizeClass = "sixteen"
+				pixlSize = 20;
+				pixlMargin = 4;
+				break;
+			case 4:
+				pixlSizeClass = "forty"
+				pixlSize = 40;
+				pixlMargin = 8;
+				break;
+
+		}
+
+		var divX = this.width();
+		var divY = this.height();
+
+		var pixlX = Math.floor( divX / ( pixlSize + pixlMargin * 2 ) ) ;
+		var pixlY = Math.floor( divY / ( pixlSize + pixlMargin * 2 ) ) ;
+
+		//console.log("This div is " + divX + " by " + divY + " pixels. We can fit " + pixlX + " pixels in the X direction and " + pixlY + " pixels in the Y direction" );
+		
+		for ( var iy = 0; iy < pixlY; iy++ ) {
+			for ( var ix = 0; ix < pixlX; ix++ ) {
+				this.append( "<div id='_"+ix+"_"+iy+"' class='pixl pixl-"+pixlSizeClass+"'></div>" ).hide().show('fast');
+			}
+		}
+
+
+
+	};
+
+}( jQuery ));
--- static/js/template.js
+++ static/js/template.js
@@ -1,5 +1,5 @@
 // Now we've configured RequireJS, we can load our dependencies and start
-define([ 'ractive', 'rv!../ractive/template'], function ( Ractive, html) {
+define([ 'ractive', 'rv!../ractive/template', 'jquery','pixl'], function ( Ractive, html, $, pixl) {
 
 
 
@@ -10,6 +10,19 @@ define([ 'ractive', 'rv!../ractive/template'], function ( Ractive, html) {
         greeting: "Hello, World"
       }
     });
+
+    $( document ).ready( function(){
+    	createPixlGrid();
+    });
+
+    $( window ).resize( function(){
+    	createPixlGrid();
+    });
+
+    function createPixlGrid() {
+    	$( "#pixl-grid" ).pixl("100%","100%", 4);
+    }
+
 
     return sampleRactive;
 
